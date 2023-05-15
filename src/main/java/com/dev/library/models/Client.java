7bf3f03f2492b3cd8dfd4d16d0b519cd8b863322
caiocipriano <caio.silvax@hotmail.com>
2023-05-15 18:19:45 -0300

    feat:Client Model

---
+++ src/main/java/com/dev/library/models/Client.java
@@ -0,0 +1,27 @@
+package com.dev.library.models;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client extends Person{
+	private static final long serialVersionUID = 1L;
+	
+	@NotNull(message="O campo de data não pode ser vazio")
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private Date dateBorn;
+	
+	 @Builder
+	 public Client(Integer id, String name, String cpf, String phone, Date dateBorn) {
+	      super();
+	      this.dateBorn = dateBorn;
+	  }
+}
