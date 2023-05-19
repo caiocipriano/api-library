83738eaae8dc3da6c7a1826b7e8a3895ca2584af
caiocipriano <caio.silvax@hotmail.com>
2023-05-18 22:05:45 -0300

    Feat: Removing validation

--- src/main/java/com/dev/library/models/Person.java
+++ src/main/java/com/dev/library/models/Person.java
@@ -15,21 +15,17 @@ public abstract class Person implements Serializable{
     private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column(length=50)
-    @NotBlank(message = "Nome da Pessoa deve ser preenchido")
 	@Size(min = 2, max = 50, message = "Nome da Pessoa deve ter entre 2 e 50 letras")
 	private String name;
 	
 	
     @Column(length = 50)
-    @NotBlank(message = "CPF da Pessoa deve ser preenchido")
-    @Pattern(regexp="\\d{3}.\\d{3}.\\d{3}-\\d{2}", message = "CPF da Pessoa deve seguir o padrão NNN.NNN.NNN-NN")
 	private String cpf;
     
-    @NotBlank(message = "Numero da Pessoa deve ser preenchido")
     @Size(min = 1, max = 11, message = "Número com padrão 11 dígitos")
 	private String phone;
 }
