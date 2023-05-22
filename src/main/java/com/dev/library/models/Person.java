0ffa3fd1a916aae22dd4a5937f10c51c91c9b944
caiocipriano <caio.silvax@hotmail.com>
2023-05-22 17:44:50 -0300

    Feat:Removing Lombok Annotations

--- src/main/java/com/dev/library/models/Person.java
+++ src/main/java/com/dev/library/models/Person.java
@@ -6,26 +6,69 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
-@MappedSuperclass
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Person implements Serializable{	
-	
-    private static final long serialVersionUID = 1L;
-	
+		
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	private Integer id;
 	
 	@Column(length=50)
-	@Size(min = 2, max = 50, message = "Nome da Pessoa deve ter entre 2 e 50 letras")
 	private String name;
 	
 	
     @Column(length = 50)
 	private String cpf;
     
-    @Size(min = 1, max = 11, message = "Número com padrão 11 dígitos")
 	private String phone;
+	
+
+	public Person( String name, String cpf, String phone) {
+		this.name = name;
+		this.cpf = cpf;
+		this.phone = phone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
+
