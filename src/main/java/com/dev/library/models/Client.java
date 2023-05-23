23bd8402843934d682747cf49e3435419e247140
caiocipriano <caio.silvax@hotmail.com>
2023-05-23 15:42:53 -0300

    Feat: Getters and Setters Constructors and Jakarta annotations

--- src/main/java/com/dev/library/models/Client.java
+++ src/main/java/com/dev/library/models/Client.java
@@ -1,40 +1,42 @@
 package com.dev.library.models;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class Client extends Person{
-	
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
 	@NotNull(message="O campo de data não pode ser vazio")
 	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date dateBorn;
 	
-	
 
-	public Client(String name, String cpf, String phone,Date dateBorn) {
-		super(name, cpf, phone);
-		this.dateBorn=dateBorn;
-	}
+	public Client(Integer id,String name,String cpf,String street, int number, Date dateBorn) {
+		super(id, name, cpf, street, number);
+		this.dateBorn = dateBorn;	}
+
 
 	public Date getDateBorn() {
 		return dateBorn;
 	}
+
 
 	public void setDateBorn(Date dateBorn) {
 		this.dateBorn = dateBorn;
 	}
 
 
-	 
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
 }
