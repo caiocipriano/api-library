fd8d30c5d978ca4f84b6b965420ddaa1340e754d
caiocipriano <caio.silvax@hotmail.com>
2023-05-29 20:52:03 -0300

    Feat: Add Constructors and Getters/Setters

--- src/main/java/com/dev/library/models/Reserve.java
+++ src/main/java/com/dev/library/models/Reserve.java
@@ -19,15 +19,77 @@ public class Reserve implements Serializable{
 	private Integer id;
 	
 	@JsonFormat(pattern="yyyy-MM-dd")
-	private Date dateBorn;
+	private Date date;
 	
 	@Digits(integer=1, fraction=0, message="Status da Reserva deve ser preechido")
 	private Integer status;
 	
-	@NotNull
+	
+	@NotNull(message = "A Fita da Reserva deve ser preenchida")
+	@ManyToOne
+	@JoinColumn(name="tape_id")
+	private Tape tape;
+	
+	@NotNull(message = "O Cliente da Reserva deve ser preenchido")
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private Client client;
+
+	public Reserve(Integer id, Date date,
+			@Digits(integer = 1, fraction = 0, message = "Status da Reserva deve ser preechido") Integer status,
+			@NotNull(message = "A Fita da Reserva deve ser preenchida") Tape tape,
+			@NotNull(message = "O Cliente da Reserva deve ser preenchido") Client client) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.status = status;
+		this.tape = tape;
+		this.client = client;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDateBorn() {
+		return date;
+	}
+
+	public void setDateBorn(Date date) {
+		this.date = date;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Tape getTape() {
+		return tape;
+	}
+
+	public void setTape(Tape tape) {
+		this.tape = tape;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	
 		
 }
