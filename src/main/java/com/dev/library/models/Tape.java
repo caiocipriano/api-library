75e6d067553659bf4b878b1521ebae4990a0ddb3
caiocipriano <caio.silvax@hotmail.com>
2023-05-29 20:51:23 -0300

    Feat: Add Constructors and Getters/Setters

--- src/main/java/com/dev/library/models/Tape.java
+++ src/main/java/com/dev/library/models/Tape.java
@@ -28,4 +28,61 @@ public class Tape implements Serializable{
 	@JoinColumn(name="movie_id")
 	private Movie movie;
 
+
+	public Tape(Integer id, @NotNull(message = "O atributo de 'Danificada' deve ser preenchido") Boolean damaged,
+			@NotNull(message = "O atributo de 'Disponivel' deve ser preenchido") Boolean available,
+			@NotNull(message = "O Filme da Fita deve ser preenchido") Movie movie) {
+		super();
+		this.id = id;
+		this.damaged = damaged;
+		this.available = available;
+		this.movie = movie;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Boolean getDamaged() {
+		return damaged;
+	}
+
+
+	public void setDamaged(Boolean damaged) {
+		this.damaged = damaged;
+	}
+
+
+	public Boolean getAvailable() {
+		return available;
+	}
+
+
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
