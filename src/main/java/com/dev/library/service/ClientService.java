d4f4e895f445e7841d727b2800054b53e79b7814
caiocipriano <caio.silvax@hotmail.com>
2023-06-07 21:10:21 -0300

    Feat: Client Endpoint's in Service

--- src/main/java/com/dev/library/service/ClientService.java
+++ src/main/java/com/dev/library/service/ClientService.java
@@ -1,11 +1,15 @@
 package com.dev.library.service;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dev.library.models.Client;
 import com.dev.library.repositories.ClientRepository;
+import com.dev.library.service.exeptions.DataIntegrityException;
+import com.dev.library.service.exeptions.ObjectNotFoundException;
 
 @Service
 public class ClientService {
@@ -13,6 +17,9 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	  public Collection<Client> findAll() {
+	        return repository.findAll();
+	    }
 
 	public Client post( Client client){ 
 		try {
@@ -21,5 +28,37 @@ public class ClientService {
         throw new DataIntegrityViolationException("Campo(s) obrigatório(s) do Cliente não foi(foram) preenchido(s): Bairro ou Telefone");
     }
   }
+	
+	public Client update(Client client) {
+    	Client obj = findById(client.getId());
+    	if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Tipo:" + Client.class.getName());
+    	}
+    	try {
+			return repository.save(client);
+		}catch(DataIntegrityException e) {
+            throw new DataIntegrityViolationException("Não é possível atualizar um cliente, verifique os dados!");
+		}
+	}
+	
+	public Client findById(Integer id) {
+		Client obj = repository.findById(id).get();
+		if(obj ==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Tipo:" + Client.class.getName());
+		}
+		return obj;
+	}
+	
+	public void delete(Integer id) {
+		Client obj =  findById(id);
+		if(obj ==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:"+ id +",Tipo:" + Client.class.getName());
+		}
+        try {
+   		 repository.deleteById(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir um Cliente associado a Reservas ou Empréstimos!");
+        }
+	}
 }
 
